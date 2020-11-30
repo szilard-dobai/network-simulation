d8725df0e544fa16f6b034b9fb93cab757940dd0
Szilard DOBAI <dobai_szilard@yahoo.com>
2020-11-30 19:27:48 +0200

    fixed untracked files

--- out/clang-release/Scheduler.o.d
+++
@@ -1,4 +0,0 @@
-out/clang-release//Scheduler.o: Scheduler.cc \
-  F:/OMNeT++/omnetpp-5.6/include\omnetpp.h
-
-F:/OMNeT++/omnetpp-5.6/include\omnetpp.h:
--- out/clang-release/Sink.o.d
+++
@@ -1,4 +0,0 @@
-out/clang-release//Sink.o: Sink.cc \
-  F:/OMNeT++/omnetpp-5.6/include\omnetpp.h
-
-F:/OMNeT++/omnetpp-5.6/include\omnetpp.h:
